fcf7596010e09c80ce052d184c3b8869a5f96da8
Emilio Alvarez <emilioa84@yahoo.com>
2015-11-08 21:09:46 -0800

    added ruby code

--- erb_example.rb
+++ erb_example.rb
@@ -3,3 +3,11 @@ require 'erb'
 x = 42
 template = ERB.new "The value of x is: <%= x %>"
 puts template.result(binding)
+
+teams = ['Chargers', 'Panthers','Vikings']
+
+puts teams
+
+teams.each do |football|
+	puts "Win #{football}"
+end	
